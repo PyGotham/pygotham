cd1d5d424da9f570f6c666cbe882a0748f1a23d1
Andy Dirnberger <dirn@dirnonline.com>
2014-05-21 02:01:29 -0400

    Add initial registration information
    This new registration information page will contain details about costs
    of registering and the financial aid available. Prices here are not
    necessarily finalized and may still change.

---
+++ pygotham/frontend/registration.py
@@ -0,0 +1,12 @@
+"""PyGotham registration information."""
+
+from flask import Blueprint
+
+from pygotham.frontend import direct_to_template
+
+__all__ = 'blueprint',
+
+blueprint = Blueprint('registration', __name__, url_prefix='/registration')
+
+direct_to_template(
+    blueprint, '/information', template='registration/information.html')
