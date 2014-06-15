75cd06577d84f877990a3ee543e0d62d5a9440dc
Andy Dirnberger <dirn@dirnonline.com>
2014-06-15 14:39:15 -0400

    Limit supported HTML tags for reStructuredText
    Because users are allowed to enter text that will be converted to HTML,
    we need to be wary of the tags that are allowed. We don't want anyone to
    override styles on the page or inject (harmful) JavaScript. The `bleach`
    library can be used to help limit what it's allowed.
    
    `bleach` can be a little too aggressive though, so the list of supported
    tags needs to be expanded to included headings and paragraphs.

--- pygotham/frontend/__init__.py
+++ pygotham/frontend/__init__.py
@@ -4,6 +4,7 @@ from docutils import core
 from functools import wraps
 import os
 
+import bleach
 from flask import render_template
 from flask.ext.assets import Bundle, Environment
 from flask.ext.foundation import Foundation
@@ -12,6 +13,8 @@ from pygotham import factory
 from pygotham.events import get_current as get_current_event
 
 __all__ = 'create_app', 'route'
+
+_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
 
 
 def create_app(settings_override=None):
@@ -64,7 +67,8 @@ def create_app(settings_override=None):
     @app.template_filter('rst')
     def rst_to_html(value):
         parts = core.publish_parts(source=value, writer_name='html')
-        return parts['body_pre_docinfo'] + parts['fragment']
+        return bleach.clean(
+            parts['body_pre_docinfo'] + parts['fragment'], tags=_ALLOWED_TAGS)
 
     if not app.debug:
         for e in (404, 500):
