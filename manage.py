698f1cdcf3a62c1a72fc346292e72d3cd1e82f5c
Andy Dirnberger <dirn@dirnonline.com>
2015-04-18 22:29:19 -0400

    Add management command to create admin users
    One of the biggest points of confusion when setting up a new
    installation of this project is how to create an admin user. The process
    requires using the `create_user` management command and then issuing SQL
    statements to create the admin role and add it to the new user.
    
    This new management command takes care of all of that.
    
    Because of the overlap in functionality between the `create_user` and
    `create_admin` commands, a new class, `RegisterUserMixin`, is being
    added.

--- manage.py
+++ manage.py
@@ -4,9 +4,10 @@ from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager
 
 from pygotham.factory import create_app
-from pygotham.manage import CreateUser
+from pygotham.manage import CreateAdmin, CreateUser
 
 manager = Manager(create_app(__name__, ''))
+manager.add_command('create_admin', CreateAdmin())
 manager.add_command('create_user', CreateUser())
 manager.add_command('db', MigrateCommand)
 
