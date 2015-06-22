b73a41750ffde1d0279db9b7f6e97703238f52e9
Andy Dirnberger <dirn@dirnonline.com>
2015-06-21 23:53:31 -0400

    Add calls to action
    The original version of the 2015 home page featured a call to action
    block. Unfortunately there was no way to control the call to action
    outside of changing the template. A new model is being added to allow
    for them to be controlled through the admin.
    
    There will be some overlap between calls to action and announcements,
    but calls to action will likely be kept around longer than an
    announcement will be the most recent.

--- pygotham/news/__init__.py
+++ pygotham/news/__init__.py
@@ -3,12 +3,12 @@
 import arrow
 
 from pygotham.core import db
-from pygotham.news.models import Announcement
+from pygotham.news.models import Announcement, CallToAction
 
-__all__ = ('get_active',)
+__all__ = ('get_active_announcements', 'get_active_call_to_action')
 
 
-def get_active():
+def get_active_announcements():
     """Get the active announcements."""
     now = arrow.utcnow().to('America/New_York').naive
 
@@ -17,3 +17,14 @@ def get_active():
         Announcement.active == True,
         Announcement.published < now,
     ).order_by(db.desc(Announcement.published)).all()
+
+
+def get_active_call_to_action():
+    """Return the active call to action."""
+    now = arrow.utcnow().to('America/New_York').naive
+
+    return CallToAction.query.current.filter(
+        CallToAction.active == True,
+        CallToAction.begins < now,
+        db.or_(CallToAction.ends > now, CallToAction.ends == None),
+    ).order_by(CallToAction.begins, db.desc(CallToAction.ends)).first()
