bc567530a5bce837c063c2b1747736bf4a2b08b9
Jon Banafato <jon@jonafato.com>
2016-04-03 22:19:34 -0400

    Appease nginx and letsencrypt

--- server/ssl-scripts/generate-certificate.sh
+++ server/ssl-scripts/generate-certificate.sh
@@ -5,7 +5,7 @@ DOMAINS="-d pygotham.org -d www.pygotham.org"
 # Add the yearly subdomains
 DOMAINS="$DOMAINS -d 2014.pygotham.org -d 2015.pygotham.org -d 2016.pygotham.org -d 2017.pygotham.org"
 # Store the challenge in a tmp dir (only needed when letsencrypt is running)
-WEBROOT_PATH="/tmp/letsencrypt-auto"
+WEBROOT_PATH="/srv/letsencrypt-auto"
 mkdir -p $WEBROOT_PATH
 letsencrypt --renew certonly --webroot --webroot-path $WEBROOT_PATH --agree-tos $DOMAINS
 service nginx reload
